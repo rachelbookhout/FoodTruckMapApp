f82d5c7e814f18d2b5f4d6e519f123f1be7d12b4
rachelbookhout <rbookhou@gmail.com>
2016-05-01 14:26:52 -0400

    markers work. need debugging and addition of info

--- app/controllers/foodtrucks_controller.rb
+++ app/controllers/foodtrucks_controller.rb
@@ -6,34 +6,31 @@ class FoodtrucksController < ApplicationController
   # else
     @trucks = Schedule.search(params[:search])
     #trucks is within the @trucks array, need to query off its food truck id
-    @info = [];
-    @trucks.each do |truck|
-      @info << Foodtruck.where(id: "#{truck.foodtruck_id}")
-    end
-
+    binding.pry
 
 
   # end
-  #   @geojson = []
-  #   @trucks.each do |truck|
-  #     @geojson << {
-  #     type: 'Feature',
-  #     geometry: {
-  #     type: 'Point',
-  #     coordinates: [truck.longitude, truck.latitude]
-  #     },
-  #     properties: {
-  #     cuisine: , #add to this
-  #     schedule: , #add to this
-  #     :'marker-color' => '#00607d',
-  #     :'marker-symbol' => 'circle',
-  #     :'marker-size' => 'medium'
-  #     }
-  #   }
-  #   end
-  #   respond_to do |format|
-  #     format.html
-  #     format.json { render json: @geojson }
-  #   end
+      @geojson = []
+      @trucks.each do |truck|
+        @truckinfo = Foodtruck.where(id: "#{truck.foodtruck_id}")
+        @location = Location.where(id: "#{truck.location_id}")
+        @geojson << {
+        type: 'Feature',
+        geometry: {
+        type: 'Point',
+        coordinates: [@location[0]["longitude"], @location[0]["latitude"]]
+        },
+        properties: {
+        cuisine: "Cuisine!",
+        :'marker-color' => '#00607d',
+        :'marker-symbol' => 'circle',
+        :'marker-size' => 'medium'
+      }
+    }
+    end
+    respond_to do |format|
+      format.html
+      format.json { render json: @geojson }
+    end
    end
 end
