7170932a85caab0b6f17ec81da4b58fe7cbf949a
rachelbookhout <rbookhou@gmail.com>
2016-05-07 14:30:58 -0400

    dropped old tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,49 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506011117) do
+ActiveRecord::Schema.define(version: 20160507182034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "cuisines", force: :cascade do |t|
-    t.integer "cuisine_id", null: false
-    t.string  "name"
-  end
-
   create_table "dayofweeks", force: :cascade do |t|
     t.string "day", null: false
-  end
-
-  create_table "days", force: :cascade do |t|
-    t.integer "day_id",    null: false
-    t.string  "dayofweek", null: false
-  end
-
-  create_table "foodtrucks", force: :cascade do |t|
-    t.string  "name",       null: false
-    t.integer "cuisine_id"
-    t.string  "photo"
-    t.string  "url"
-    t.string  "twitter"
-  end
-
-  create_table "locations", force: :cascade do |t|
-    t.string "name",      null: false
-    t.float  "latitude",  null: false
-    t.float  "longitude", null: false
-  end
-
-  create_table "mealtimes", force: :cascade do |t|
-    t.integer "mealtime_id", null: false
-    t.string  "meal",        null: false
-  end
-
-  create_table "schedules", force: :cascade do |t|
-    t.integer "mealtime_id",  null: false
-    t.integer "day_id",       null: false
-    t.integer "foodtruck_id", null: false
-    t.integer "location_id",  null: false
   end
 
   create_table "trucks", force: :cascade do |t|
